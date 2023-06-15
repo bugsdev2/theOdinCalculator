d2c26d859e961fe66cb42eb2cd929404d4154372
BugsDev2 <msherlock246@gmail.com>
2023-06-15 17:51:09 +0530

    Included the darkmode class to affect the root element rather than the body. Also, I noticed that I'd not included the html tags in the html. how stupid of me!

--- darkMode.js
+++ darkMode.js
@@ -1,14 +1,18 @@
 const darkModeToggle = document.querySelector('.dark-mode-button');
 const darkModeImg = document.querySelector('.dark-mode-button img:nth-child(2)');
+const root = document.documentElement;
+
 let flag = false;
-console.log(darkModeImg.style.display)
+console.log(root)
 const darkModeEnable = () => {
-    document.body.classList.add('dark-mode');
+    root.classList.add('dark-mode');
+    // document.body.classList.add('dark-mode');
     darkModeImg.style.display = 'none';
 }
 
 const darkModeDisable = () => {
-    document.body.classList.remove('dark-mode');
+    root.classList.remove('dark-mode')
+    // document.body.classList.remove('dark-mode');
     darkModeImg.style.display = 'block';
 }
 
